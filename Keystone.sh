79b47441c7f1b088a01c74d19f1c3cfbaac42c1e
beeru405 <101712802+beeru405@users.noreply.github.com>
2023-10-19 09:39:55 +0530

    Create Keystone.sh

---
+++ Keystone.sh
@@ -0,0 +1,8 @@
+juju deploy --to lxd:0 --channel 2023.1/stable keystone
+
+juju deploy --channel 8.0/stable mysql-router keystone-mysql-router
+juju integrate keystone-mysql-router:db-router mysql-innodb-cluster:db-router
+juju integrate keystone-mysql-router:shared-db keystone:shared-db
+
+juju integrate keystone:identity-service neutron-api:identity-service
+juju integrate keystone:certificates vault:certificates
