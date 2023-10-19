1bf322bcc5df0e1003580a823c0da8ba1a5d3cee
beeru405 <101712802+beeru405@users.noreply.github.com>
2023-10-19 09:42:51 +0530

    Create Placement.sh

---
+++ Placement.sh
@@ -0,0 +1,9 @@
+juju deploy --to 3 --channel 2023.1/stable placement
+
+juju deploy --channel 8.0/stable mysql-router placement-mysql-router
+juju integrate placement-mysql-router:db-router mysql-innodb-cluster:db-router
+juju integrate placement-mysql-router:shared-db placement:shared-db
+
+juju integrate placement:identity-service keystone:identity-service
+juju integrate placement:placement nova-cloud-controller:placement
+juju integrate placement:certificates vault:certificates
