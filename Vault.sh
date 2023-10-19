954982010e90447a41d356b2eecd35e9ace870bb
beeru405 <101712802+beeru405@users.noreply.github.com>
2023-10-19 09:37:29 +0530

    Create Vault.sh

---
+++ Vault.sh
@@ -0,0 +1,7 @@
+juju deploy --to 3 --channel 1.8/stable vault
+
+juju deploy --channel 8.0/stable mysql-router vault-mysql-router
+juju integrate vault-mysql-router:db-router mysql-innodb-cluster:db-router
+juju integrate vault-mysql-router:shared-db vault:shared-db
+
+juju integrate mysql-innodb-cluster:certificates vault:certificates
