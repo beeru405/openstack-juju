59de1e9ceea5384521f327b58d7c004a746061a3
beeru405 <101712802+beeru405@users.noreply.github.com>
2023-10-19 09:47:11 +0530

    Update Ceph monitor.sh

--- Ceph monitor.sh
+++ Ceph monitor.sh
@@ -1,1 +1,7 @@
 juju deploy -n 3 --to 0,1,2 --channel quincy/stable --config ceph-mon.yaml ceph-mon
+
+juju integrate ceph-mon:osd ceph-osd:mon
+juju integrate ceph-mon:client nova-compute:ceph
+juju integrate ceph-mon:client glance:ceph
+
+
